3155e8f4c614c9fb5c3541de97ee4cc8aacbc040
Sakib <sakib.hstu.bd@gmail.com>
2023-05-19 12:13:19 +0600

    category view detail route set

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -30,15 +30,19 @@ async function run() {
         const categoriesCollection = client.db('CarZoneKids').collection('categories');
         // console.log(categoriesCollection)
 
-         //------ services routes --------
-         app.get('/categories', async (req, res) => {
+        //------ categories  routes --------
+        app.get('/categories', async (req, res) => {
             const cursor = categoriesCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
 
-
-
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await categoriesCollection.findOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
