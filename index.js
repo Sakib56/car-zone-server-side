b7dba6cc5b182aa905fb764409fe7cf9f3f88cad
Sakib <sakib.hstu.bd@gmail.com>
2023-05-19 18:52:56 +0600

    searchBytoys route set

--- index.js
+++ index.js
@@ -27,12 +27,18 @@ async function run() {
         await client.connect();
 
 
-        const categoriesCollection = client.db('CarZoneKids').collection('allToys');
+        const toysCollection = client.db('CarZoneKids').collection('allToys');
         // console.log(categoriesCollection)
+
+        // Creating index on two fields
+        const indexKeys = { toy_name: 1 }; // Replace field1 and field2 with your actual field names
+        const indexOptions = { name: "toy_name" }; // Replace index_name with the desired index name
+        const result = await toysCollection.createIndex(indexKeys,indexOptions)
+        console.log(result);
 
         //------ categories  routes --------
         app.get('/allToys', async (req, res) => {
-            const cursor = categoriesCollection.find()
+            const cursor = toysCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
@@ -40,9 +46,23 @@ async function run() {
         app.get('/allToys/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
-            const result = await categoriesCollection.findOne(query)
+            const result = await toysCollection.findOne(query)
             res.send(result)
         })
+
+
+        app.get("/getToysByText/:text", async (req, res) => {
+            const text = req.params.text;
+            const result = await toysCollection
+                .find({
+                    $or: [
+                        { toy_name: { $regex: text, $options: "i" } }
+                    ],
+                })
+                .toArray();
+            res.send(result);
+        });
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
